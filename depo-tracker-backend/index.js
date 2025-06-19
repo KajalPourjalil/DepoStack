002dbd607db57717570b2cb39f75f6ad38ce3eaf
KajalPourjalil <rina.skyler@yahoo.com>
2025-06-19 23:07:19 +0300

    connecting to mongoose

--- depo-tracker-backend/index.js
+++ depo-tracker-backend/index.js
@@ -13,3 +13,16 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+}   
+);
